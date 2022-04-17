063f1be472635a0f5094f2c2b4658bcb40950a1c
Heverton Reis <heverton.reis04@gmail.com>
2022-04-17 16:40:33 -0300

    doc: Exercitando consultas

--- AluraTunes/AluraTunes/Program.cs
+++ AluraTunes/AluraTunes/Program.cs
@@ -1,12 +1,127 @@
-﻿using System;
+﻿using AluraTunes.Data;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Xml.Linq;
 
 namespace AluraTunes
 {
     internal class Program
     {
         static void Main(string[] args)
+        {
+            using (var contexto = new AluraTunesEntities() )
+            {
+                var textoBusca = "Led";
+
+                var query = from a in contexto.Artistas
+                            join alb in contexto.Albums
+                            on a.ArtistaId equals alb.ArtistaId
+                            where a.Nome.Contains(textoBusca)
+                            select new
+                            {
+                                NomeArtista = a.Nome,
+                                NomeAlbum = alb.Titulo
+                            };
+
+                foreach (var item in query)
+                {
+                    Console.WriteLine("{0}\t{1}", item.NomeArtista, item.NomeAlbum);
+                }
+
+                var query2 = contexto.Artistas.Where(a => a.Nome.Contains(textoBusca));
+
+                Console.WriteLine();
+                foreach (var artista in query2)
+                {
+                    Console.WriteLine("{0}\t{1}", artista.ArtistaId, artista.Nome);
+                }
+
+                var query3 = from alb in contexto.Albums
+                             where alb.Artista.Nome.Contains(textoBusca)
+                             select new
+                             {
+                                 NomeArtista = alb.Artista.Nome,
+                                 NomeAlbum = alb.Titulo
+                             };
+
+                Console.WriteLine();
+                foreach (var album in query3)
+                {
+                    Console.WriteLine("{0}\t{1}", album.NomeArtista, album.NomeAlbum);
+                }
+            }
+            //LinQToEntities();
+            //QueryXML();
+            //QuerysBasicas();
+        }
+
+        private static void LinQToEntities()
+        {
+            using (var contexto = new AluraTunesEntities())
+            {
+                var query = from g in contexto.Generos
+                            select g;
+
+                foreach (var genero in query)
+                {
+                    Console.WriteLine("{0}\t{1}", genero.GeneroId, genero.Nome);
+                }
+
+                var faixaEGenero = from g in contexto.Generos
+                                   join f in contexto.Faixas
+                                   on g.GeneroId equals f.GeneroId
+                                   select new
+                                   {
+                                       f,
+                                       g
+                                   };
+
+                faixaEGenero = faixaEGenero.Take(10);
+
+                contexto.Database.Log = Console.WriteLine;
+
+                Console.WriteLine();
+
+                foreach (var item in faixaEGenero)
+                {
+                    Console.WriteLine("{0}\t{1}", item.f.Nome, item.g.Nome);
+                }
+
+            }
+        }
+
+        private static void QueryXML()
+        {
+            XElement root = XElement.Load(@"C:\Users\Heverton Reis\Documents\Curso-Alura-LinQ\AluraTunes\AluraTunes\Data\AluraTunes.xml");
+
+            var queryXML =
+                from g in root.Element("Generos").Elements("Genero")
+                select g;
+
+            foreach (var genero in queryXML)
+            {
+                Console.WriteLine("{0}\t{1}", genero.Element("GeneroId").Value, genero.Element("Nome").Value);
+            }
+
+            Console.WriteLine();
+            var query = from g in root.Element("Generos").Elements("Genero")
+                        join m in root.Element("Musicas").Elements("Musica")
+                        on g.Element("GeneroId").Value equals m.Element("GeneroId").Value
+                        select new
+                        {
+                            Musica = m.Element("Nome").Value,
+                            Genero = g.Element("Nome").Value
+                        };
+
+            Console.WriteLine();
+            foreach (var musicaEGenero in query)
+            {
+                Console.WriteLine("{0}\t{1}", musicaEGenero.Musica, musicaEGenero.Genero);
+            }
+        }
+
+        private static void QuerysBasicas()
         {
             var generos = new List<Genero>
             {
@@ -64,9 +179,9 @@ namespace AluraTunes
             }
 
             var listarReggae = from m in musicas
-                                    join g in generos on m.GeneroId equals g.Id
-                                    where g.Nome == "Reggae"
-                                    select new { m, g };
+                               join g in generos on m.GeneroId equals g.Id
+                               where g.Nome == "Reggae"
+                               select new { m, g };
 
             Console.WriteLine();
             foreach (var musica in listarReggae)
